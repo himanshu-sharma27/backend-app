b2a8420d5e75370f8f4b188104a84bcf828a4450
himanshu-sharma27 <himanshu.madduri@gmail.com>
2026-03-10 14:16:30 +0530

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,9 +27,8 @@ const editProductForm = async (req,res) => {
     res.render("products/edit", {product});
 }
 
-const saveProduct = async (req,res) => {
-    const id = req.body.id;
-    
+const saveProduct = async (req, res) => {
+    const id = req.params.id; 
     await productModel.findByIdAndUpdate(id, req.body);
     res.redirect("/products");
 }
